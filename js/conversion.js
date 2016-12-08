2a2f3d015a95fda09a7925f64ae4a59b2d089a20
atomicwest <ogjdne@gmail.com>
2016-12-08 14:58:55 -0800

    initial commit

---
+++ js/conversion.js
@@ -0,0 +1,13 @@
+var input = 23
+var inputsys = "decimal"
+var outputsys = "binary"
+var output = ""
+
+function fromdecimal(value){
+  if inputsys=="decimal"{
+    switch(outputsys){
+      case "binary":
+
+    }
+  }
+}
